22db7d05efc967ed1492d32ef1055f4b05600e11
H0tt4ru <stenlieangelonathan@gmail.com>
2024-08-10 10:48:16 +0700

    setting up a bit

--- components/Draggable.tsx
+++ components/Draggable.tsx
@@ -1,29 +1,29 @@
 import Image from "next/image";
 import Ellypsis from "@/public/svgs/ellypsis-vertical.svg";
-import Edit from "@/public/svgs/edit.svg"
+import Edit from "@/public/svgs/edit.svg";
 
 interface Text {
-  parameter: string;
+    parameter: string;
 }
 
 const Draggable: React.FC<Text> = ({ parameter }) => {
-  return (
-    <>
-      <div className="bg-Neutral400 w-[261px] h-[52px] rounded-[15px] flex items-center px-3">
-        <Image
-          className="w-[28px] cursor-pointer mr-3"
-          alt="Ellypsis"
-          src={Ellypsis}
-        />
-        <p>{parameter}</p>
-        <Image
-          className="w-[28px] cursor-pointer ml-auto"
-          alt="Edit"
-          src={Edit}
-        />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="flex h-[52px] w-[261px] items-center rounded-[15px] bg-Neutral400 px-3">
+                <Image
+                    className="mr-3 w-[28px] cursor-pointer"
+                    alt="Ellypsis"
+                    src={Ellypsis}
+                />
+                <p>{parameter}</p>
+                <Image
+                    className="ml-auto w-[28px] cursor-pointer"
+                    alt="Edit"
+                    src={Edit}
+                />
+            </div>
+        </>
+    );
 };
 
 export default Draggable;
